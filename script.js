f18113f5b1eefa7d7851d5f0f0c37fe840f75237
A-childs-encyclopedia <84061672+A-childs-encyclopedia@users.noreply.github.com>
2025-02-21 13:41:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function CheckHouse(){
     let indexn = document.getElementById("inum");
     let indexnum = indexn.value;
-    let errors = 0
     console.log("indexnum is", indexnum);
     console.log("indexn is", indexn);
     
@@ -15,11 +14,7 @@ function CheckHouse(){
         window.alert("Al-Azhar")
     } else if (indexnum == "") {
         window.alert("Please input a valid index number")
-        errors += 1
-        console.log(errors)
     } else {
         window.alert("Please input a valid index number")
-        errors += 1
-        console.log(errors)
     }
 }
