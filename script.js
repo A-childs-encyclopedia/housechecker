01e61368c9e245b0c914d783c5e968ad8c8d86ac
A-childs-encyclopedia <84061672+A-childs-encyclopedia@users.noreply.github.com>
2025-02-21 13:35:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function CheckHouse(){
     let indexn = document.getElementById("inum");
     let indexnum = indexn.value;
+    let errors = 0
     console.log("indexnum is", indexnum);
     console.log("indexn is", indexn);
     
@@ -10,7 +11,11 @@ function CheckHouse(){
         window.alert("Nizamiya")
     } else if (indexnum % 4 == 2) {
         window.alert("Al-Zahra")
-    } else {
+    } else if (indexnum % 4 == 3) {
         window.alert("Al-Azhar")
+    } else if (indexnum == "") {
+        window.alert("Please input a valid index number")
+        errors += 1
+        consol.log(errors)
     }
 }
