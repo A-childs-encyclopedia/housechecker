f4598fb2b7f0b7e2fd0376f0b74b9204b4262e7d
A-childs-encyclopedia <abdhllah005@gmail.com>
2025-02-21 20:31:26 +0530

    change

--- script.js
+++ script.js
@@ -22,47 +22,6 @@ self.addEventListener("install", (e) => {
     console.log("[Service Worker] Install");
 });
 
-const cacheName = "checkhouse-v1";
-const appShellFiles = [
-  "/index.html",
-  "/script.js",
-  "/style.css",
-  "/logo.jpg",
-  "/logo.webp",
-  "/manifest.json"
-];
-
-self.addEventListener("install", (e) => {
-    console.log("[Service Worker] Install");
-    e.waitUntil(
-      (async () => {
-        const cache = await caches.open(cacheName);
-        console.log("[Service Worker] Caching all: app shell and content");
-        await cache.addAll(contentToCache);
-      })(),
-    );
-});
-
-self.addEventListener("fetch", (e) => {
-    console.log(`[Service Worker] Fetched resource ${e.request.url}`);
-});
-
-self.addEventListener("fetch", (e) => {
-    e.respondWith(
-      (async () => {
-        const r = await caches.match(e.request);
-        console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-        if (r) {
-          return r;
-        }
-        const response = await fetch(e.request);
-        const cache = await caches.open(cacheName);
-        console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-        cache.put(e.request, response.clone());
-        return response;
-      })(),
-    );  
-});
 
 
   
