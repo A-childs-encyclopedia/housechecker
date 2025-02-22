c7bf80be7850bde98427104b25f707c672da2397
A-childs-encyclopedia <abdhllah005@gmail.com>
2025-02-22 07:47:47 +0530

    Revert "pwa fix"
    This reverts commit 59d35fb16bd1bc0955b0c6f5e1256210fd34508d.

--- script.js
+++ script.js
@@ -16,16 +16,15 @@ function checkHouse(){
 }
 
 
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load' , () => {
-        navigator.serviceWorker.register('/sw.js')
-                  .then(registration => {
-            console.log('Service Worker registered with scope:' , registration.scope);
-        }).catch(error => {
-            console.log('Service Worker registration failed:' , error);
-        });
-    });
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("./sw.js");
 }
+
+self.addEventListener("install", (e) => {
+    console.log("[Service Worker] Install");
+});
+
+
 
   
   
